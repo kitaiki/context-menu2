c15b2e4f1c3c7b90abf3cec71d5f1786b706ca69
kitaiki <oh0425@gmail.com>
2025-10-12 22:56:15 +0900

    차선모달추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MapComponent from './components/MapComponent';
+import ModalPage from './pages/ModalPage';
 import './App.css';
 
 const App: React.FC = () => {
-
-
   return (
-    <div className="app">
-
-      <main className="app-main">
-        {/* 지도 컴포넌트 */}
-        <MapComponent
-          center={[126.9780, 37.5665]} // 서울 중심
-          zoom={13}
-        />
-      </main>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <div className="app">
+            <main className="app-main">
+              {/* 지도 컴포넌트 */}
+              <MapComponent
+                center={[126.9780, 37.5665]} // 서울 중심
+                zoom={13}
+              />
+            </main>
+          </div>
+        } />
+        <Route path="/modal" element={<ModalPage />} />
+      </Routes>
+    </Router>
   );
 };
 
